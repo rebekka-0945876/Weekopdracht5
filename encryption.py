1b89b5acd6caa74c9f1719aa3d697a1f28cae357
rebekka-0945876 <143963944+rebekka-0945876@users.noreply.github.com>
2024-10-13 19:33:16 +0200

    feat: added cryptography

--- encryption.py
+++ encryption.py
@@ -1,3 +1,9 @@
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+cipher = Fernet(key)
+
+
 def main_menu():
     print("Welkom bij de Symetrische Encryptie en Decryptie App!")
     print("Welke functie wil je uitvoeren?")
@@ -11,18 +17,21 @@ def main_menu():
 
 def encrypt():
     print("U heeft gekozen voor encryptie.")
-    # put encryption code here
     text = input("Voer nu de tekst in die u wilt versleutelen: ")
     print(f'De tekst die u heeft ingevoerd is: {text}')
     print("Deze tekst is nu versleuteld.")
+    print(f'De versleutelde tekst is: {cipher.encrypt(text.encode())}')
 
 
 def decrypt():
     print("U heeft gekozen voor decryptie.")
-    # put decryption code here
     text = input("Voer nu de versleutelde tekst in die u wilt ontsleutelenn: ")
     print(f'De versleutelde tekst die u heeft ingevoerd is: {text}')
-    print("Deze tekst is nu ontsleuteld.")
+    try:
+        decrypted_message = cipher.decrypt(text.encode()).decode()
+        print(f"De ontsleutelde tekst is: {decrypted_message}")
+    except Exception as e:
+        print(f"Er is een fout opgetreden tijdens het ontsleutelen: {e}")
 
 
 if __name__ == "__main__":
