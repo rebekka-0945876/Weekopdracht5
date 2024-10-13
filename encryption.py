723bc37c1779a90aaaed3dcefb8b5f2b48c318de
rebekka-0945876 <143963944+rebekka-0945876@users.noreply.github.com>
2024-10-13 19:53:27 +0200

    Feat: key gets saved to a file, so that when you restart the app you can still decrypt the messages.

--- encryption.py
+++ encryption.py
@@ -1,12 +1,35 @@
 from cryptography.fernet import Fernet
+import os
 
-key = Fernet.generate_key()
+
+def load_or_generate_key():
+    if os.path.exists("secret.key"):
+        print("Sleutelbestand gevonden, laden...")
+        try:
+            with open("secret.key", "rb") as key_file:
+                key = key_file.read()
+                print("Sleutel succesvol geladen.")
+                return key
+        except Exception as e:
+            print(f"Er is een fout opgetreden bij het laden van de sleutel: {e}")
+    else:
+        print("Geen sleutelbestand gevonden, nieuwe sleutel genereren...")
+        key = Fernet.generate_key()
+        try:
+            with open("secret.key", "wb") as key_file:
+                key_file.write(key)
+                print("Nieuwe sleutel succesvol opgeslagen.")
+        except Exception as e:
+            print(f"Er is een fout opgetreden bij het opslaan van de sleutel: {e}")
+        return key
+
+
+key = load_or_generate_key()
 cipher = Fernet(key)
 
 
 def main_menu():
-    print("Welkom bij de Symetrische Encryptie en Decryptie App!")
-    print("Welke functie wil je uitvoeren?")
+    print("Welke functie wilt u  uitvoeren?")
     print("1. Encryptie")
     print("2. Decryptie")
     print("3. App verlaten")
@@ -16,7 +39,7 @@ def main_menu():
 
 
 def encrypt():
-    print("U heeft gekozen voor encryptie.")
+    print("\nU heeft gekozen voor encryptie.")
     text = input("Voer nu de tekst in die u wilt versleutelen: ")
     print(f'De tekst die u heeft ingevoerd is: {text}')
     encrypted_message = cipher.encrypt(text.encode()).decode()
@@ -25,8 +48,8 @@ def encrypt():
 
 
 def decrypt():
-    print("U heeft gekozen voor decryptie.")
-    text = input("Voer nu de versleutelde tekst in die u wilt ontsleutelenn: ")
+    print("\nU heeft gekozen voor decryptie.")
+    text = input("Voer nu de versleutelde tekst in die u wilt ontsleutelen: ")
     print(f'De versleutelde tekst die u heeft ingevoerd is: {text}')
     try:
         decrypted_message = cipher.decrypt(text.encode()).decode()
@@ -36,6 +59,7 @@ def decrypt():
 
 
 if __name__ == "__main__":
+    print("Welkom bij de Symetrische Encryptie en Decryptie App!")
     while True:
         choice = main_menu()
         if choice == 1:
@@ -43,7 +67,7 @@ if __name__ == "__main__":
         elif choice == 2:
             decrypt()
         elif choice == 3:
-            print("De app wordt nu afgsloten.")
+            print("De app wordt nu afgesloten.")
             break
         else:
             print("Ongeldige keuze.")
